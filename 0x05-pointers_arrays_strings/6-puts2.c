41d56125a4112d73f6d1746d6ee94a807d2ca38f
ALAGA SULIAT TITILOPE <41836834+lope1999@users.noreply.github.com>
2022-09-19 17:24:01 +0100

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - print alternating chars of string
+ * @str: string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
